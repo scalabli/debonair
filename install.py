d995759f73cc5a00c9f4f852ebddb013f8c6fee5
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-07-04 21:10:35 +0300

    Update install.py

--- install.py
+++ install.py
@@ -1,17 +1,4 @@
-#!/bin/sh
-
-if [ -d /data/data/com.termux/files/usr/etc ]; then
-  conf_dir="/data/data/com.termux/files/usr/etc"
-elif [ -d /usr/etc ]; then
-  conf_dir="/usr/etc"
-elif [ -d /etc ]; then
-  conf_dir="/etc"
-fi
-
-if [ -e /usr/lib/sudo ]; then
-	sudo python3 $conf_dir/debonair_/debonair/start.py
-else
-	python3 import quo
+import quo
 import os
 import sys
 import time
@@ -23,27 +10,6 @@ def install(terms):
     time.sleep(3)
     quo.flair(f"Technically, Moses was the first person with a tablet downloading data from the cloud...", foreground="red", dim=True, bold=True)
     os.system("cd /etc/ && mkdir debonair_ && cd /etc/debonair_/ && git clone --quiet https://github.com/secretum-inc/debonair.git && cd /etc/debonair_/debonair/ && chmod +x debonair && chmod +x dbnr && mv -f  debonair /usr/bin/ && mv -f dbnr /usr/bin/ && dbnr")
-           # os.system("cd")
-            #os.system("cd /etc/debonair_/debonair && chmod +x debonair")
-            #os.system("cd")
-           # os.system("cd /etc/debonair_/debonair && chmod +x dbnr")
-           # os.system("cd")
-           # os.system("cd /etc/debonair_/debonair && mv -f -r dbnr /usr/bin")
-           # os.system("cd")
-           # os.system("cd /etc/debonair_/debonair  && mv -f -r debonair /usr/bin")
-           # os.system("clear")
-           # os.system("cd")
-           # os.system("rm -rf debonair")
-           # os.system("dbnr")
-       # else:
-           # if inp == "n" or inp== "no" or inp== "N" or inp == "NO":
-                #quo.flair(f"Quiting!!", foreground="black", background="vred")
-                #time.sleep(1)
-               # os.system("clear")
-               # os.system("exit")
 
 if __name__ == "__main__":
     install()
-
-fi
-exit
